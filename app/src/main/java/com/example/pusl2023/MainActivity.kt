9d5eafa91172a24ecb64152be216957b1ced592a
DevmithU <79127952+DevmithU@users.noreply.github.com>
2022-04-05 19:51:58 +0530

    grid view

--- app/src/main/java/com/example/pusl2023/MainActivity.kt
+++ app/src/main/java/com/example/pusl2023/MainActivity.kt
@@ -1,15 +1,47 @@
 package com.example.pusl2023
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pusl2023.model.Product
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        
+
+
+        val products = arrayListOf<Product>()
+
+        for (i in 0..100) {
+            products.add(Product("Organic Apple", "https://via.placeholder.com/350/FFFF00/ff0000", 1.99))
+
+
+        }
+        recycler_view.apply {
+            layoutManager = GridLayoutManager(this@MainActivity,2)
+            adapter = ProductsAdapter(products)
+            /*
+        *
+        *
+            recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ProductsAdapter(products)
+        }
+        * */
+
+
+        }
+
+
     }
 }
